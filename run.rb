6d21ea780b427ccd27a1080a77eff99f915a1b08
Ekow Bentsi-Enchill <ekow.bentsi.enchill@gmail.com>
2021-06-10 14:15:41 +0000

    finished belongs to lab

---
+++ run.rb
@@ -0,0 +1,12 @@
+#file was used to test code
+require "pry"
+require_relative "lib/artist.rb"
+require_relative "lib/post.rb"
+require_relative "lib/author.rb"
+
+post = Post.new
+post.title = "Hello World"
+author = Author.new
+author.name = "Uncle Bob"
+post.author = author
+binding.pry
